9330ebe262cc3a66288875ae4bf5af6883c458d3
Wang Junhua <wangjh@a-li.com.cn>
2018-03-04 23:01:37 +0800

    fix

---
+++ run_docker_tars.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -d -it --name tars --link mysql --env DBIP=mysql --env DBPort=3306 --env DBUser=root --env DBPassword=PASS -p 8080:8080 -v /c/Users/<ACCOUNT>/tars_data:/data tangramor/tars-master
